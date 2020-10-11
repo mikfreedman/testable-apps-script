f4072ae576510a0903e9aac3d41e70853b32a1b9
Mik Freedman <github@michael-freedman.com>
2020-10-10 23:38:03 -0400

    produce a banner for ease of recognition that the file is auto-generated

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const GasPlugin = require("gas-webpack-plugin");
+const TemplateBannerPlugin = require('template-banner-webpack-plugin');
 
 module.exports = {
     entry: './src/addon.js',
@@ -11,6 +12,21 @@ module.exports = {
         minimize: false
     },
     plugins: [
-        new GasPlugin()
+        new GasPlugin(),
+        new TemplateBannerPlugin({
+            banner: `This is an auto generated file, do not edit it directly!
+**********************************************************************
+        repo: {repository}
+        {name} v{version}
+        (c) 2017-{year} {author}
+        Released under the {license}.
+**********************************************************************`,
+            default(data) {
+                return {
+                    year: (new Date()).getFullYear(),
+                    license: `${data.license} License`
+                };
+            },
+        })
     ]
 };
