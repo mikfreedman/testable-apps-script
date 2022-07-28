d204ddeba7635d6f4416ebe74235035c54eac0c4
Mik Freedman <github@michael-freedman.com>
2022-07-28 13:40:44 +0000

    add banner to bundle Fixes #10

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const webpack = require('webpack');
 const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
 const gitRevisionPlugin = new GitRevisionPlugin()
 const CopyPlugin = require("copy-webpack-plugin");
+const package = require("./package.json")
+
+
 
 module.exports = {
     entry: './src/addon.js',
@@ -21,6 +24,19 @@ module.exports = {
             patterns: [
                 { from: "src/appsscript.json", noErrorOnMissing: true, },
             ],
+        }),
+        new webpack.BannerPlugin({
+            banner: () => {
+                return `This is an auto generated file, do not edit it directly!
+        **********************************************************************
+                ref: #${gitRevisionPlugin.commithash().substring(0, 7)} / ${gitRevisionPlugin.branch()}
+                repo: ${package.repository}
+                {name} v${package.version}
+                (c) 2017-${(new Date()).getFullYear()} ${package.author}
+                Released under ${package.license}.
+        **********************************************************************`
+            },
+            entryOnly: true,
         }),
     ]
 };
